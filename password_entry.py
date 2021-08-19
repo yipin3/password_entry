c662d8be21ba9114df888029eeaac2b6833ec39b
Remi Chen <yipin987@gmail.com>
2021-08-20 00:00:14 +0800

    change to while i>0

--- password_entry.py
+++ password_entry.py
@@ -1,13 +1,16 @@
 # 測試密碼程式
 
-i = 0
+i = 3
 password='a123456'
-while i < 3 :
+while i > 0:
+	i = i - 1
 	user = input('請輸入密碼: ')
 	if user == password:
 		print('正確密碼')
 		break
 	else :
-		print('密碼錯誤，你還有', 2 - i, '次機會')
-	i = i + 1
-	
+		print('密碼錯誤')
+		if i > 0:
+			print('你還有', i, '次機會')
+		else:
+			print('沒機會囉')
